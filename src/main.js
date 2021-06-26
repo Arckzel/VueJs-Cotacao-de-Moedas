90e1623740c62673c88d7976b7f443ad13b4d967
Jefferson Danilo da Silva Brito <48453882+Arckzel@users.noreply.github.com>
2021-06-26 16:47:51 -0300

    alterações nas linhas de importacao do spectre css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from '../node_modules/vue'
 import App from './App.vue'
 
-import 'spectre.css/dist/spectre.min.css'
-import 'spectre.css/dist/spectre-icons.min.css'
+import '../node_modules/spectre.css/dist/spectre.min.css'
+import '../node_modules/spectre.css/dist/spectre-icons.min.css'
 
 createApp(App).mount('#app')
