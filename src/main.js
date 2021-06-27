c9b3b8fdea308e4d5ecb9ebf9fe733eb332d2b7d
Jefferson Danilo da Silva Brito <JeffDanBrito@github.com>
2021-06-27 03:50:18 -0300

    Desisti do tentar criar a página do git hub Preciso tentar de outra forma

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-// import '../node_modules/spectre.css/dist/spectre.min.css'
-// import '../node_modules/spectre.css/dist/spectre-icons.min.css'
+import 'spectre.css/dist/spectre.min.css'
+import 'spectre.css/dist/spectre-icons.min.css'
 
-// createApp(App).mount('#app')
+createApp(App).mount('#app')
 
